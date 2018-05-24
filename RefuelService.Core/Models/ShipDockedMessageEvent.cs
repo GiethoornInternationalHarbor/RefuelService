b289e6a4cce739c4da0850d0dc094e2cda05695e
Marco Havermans <marcohavermans+github@hotmail.nl>
2018-05-24 16:52:06 +0200

    Fixed docking and undocking

---
+++ RefuelService.Core/Models/ShipDockedMessageEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefuelService.Core.Models
+{
+	public struct ShipDockedMessageEvent
+	{
+		public Guid ShipId { get; set; }
+	}
+}
